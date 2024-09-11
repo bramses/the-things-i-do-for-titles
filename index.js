714e7f1355476826175acfebb3b16ffb217058b3
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-09-11 00:01:20 -0400

    chore: Update npm dependencies

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const ogs = require("open-graph-scraper");
 const title = require("url-to-title");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.json());
 
 const API_KEY = process.env.API_KEY;
 
-app.post('/get-title', (req, res) => {
-  const { url, apiKey } = req.body;
+app.post("/get-title", (req, res) => {
+  let { url, apiKey } = req.body;
 
   if (apiKey !== API_KEY) {
-    return res.status(403).json({ error: 'Invalid API key' });
+    return res.status(403).json({ error: "Invalid API key" });
   }
 
-  const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36';
+  // Convert YouTube URL to oEmbed format
+  if (url.includes("youtube.com") || url.includes("youtu.be")) {
+    const videoId = url.split("v=")[1] || url.split("/").pop();
+    url = `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`;
+    // open yt url and get the title
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        res.json({ result: data.title });
+        return;
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "Failed to retrieve YouTube title" });
+        return;
+      });
+    return; // Add this return statement to stop further execution
+  }
 
-  const options = { url, fetchOptions: { headers: { 'user-agent': userAgent } } };
+  // if reddit url
+  if (url.includes("reddit.com")) {
+    // doesn't work...
+    // function getRedditTitle(url) {
+    //   console.log("Fetching Reddit URL:", url);
+    //   return axios.get(url)
+    //     .then(({ data }) => {
+    //       const $ = cheerio.load(data);
+    //       // write data to file
+    //       fs.writeFileSync("reddit.html", data);
+    //       const title = $("title").text();
+    //       console.log("Reddit title:", title);
+    //       return title;
+    //     })
+    //     .catch((error) => {
+    //       console.error("Error fetching the Reddit URL:", error);
+    //       return null;
+    //     });
+    // }
+
+    // getRedditTitle(url).then((title) => {
+    //   res.json({ result: title });
+    // }).catch((error) => {
+    //   console.error("Error fetching the Reddit URL:", error);
+    //   res.json({ result: "Error" });
+    // });
+
+    res.json({ result: "Reddit" });
+    return;
+  }
+
+  const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36";
+
+  const options = {
+    url,
+    fetchOptions: { headers: { "user-agent": userAgent } },
+  };
   let ogsResult = null;
   let titleResult = null;
 
@@ -26,35 +82,40 @@ app.post('/get-title', (req, res) => {
     console.log(`start ogs: ${JSON.stringify(ogsResult)}`);
     console.log(`start title: ${titleResult}`);
     if (ogsResult || titleResult) {
-      if (ogsResult.error || 'ogTitle' in ogsResult.result === false) {
-        // check if titleresult is not undefined
+      if (ogsResult.error || "ogTitle" in ogsResult.result === false) {
         if (titleResult === undefined) {
-          // check if url contains reddit
-          if (url.includes('reddit')) {
-            titleResult = 'Reddit';
+          if (url.includes("reddit")) {
+            titleResult = "Reddit";
           } else {
-            titleResult = 'Error';
+            titleResult = "Error";
           }
         }
         console.log(`titleResult: ${titleResult}`);
         res.json({ result: titleResult });
-      } else if (titleResult === 'Unsupported browser' || titleResult === undefined) {
+      } else if (
+        titleResult === "Unsupported browser" ||
+        titleResult === undefined ||
+        titleResult === "Error: Invalid content-type"
+      ) {
         console.log(`ogsResult: ${JSON.stringify(ogsResult)}`);
-        res.json({ result: ogsResult.result.ogTitle + " - " + ogsResult.result.ogDescription });
+        res.json({
+          result:
+            ogsResult.result.ogTitle + " - " + ogsResult.result.ogDescription,
+        });
       } else {
         const ogsLength = ogsResult.result.ogTitle.length;
         const titleLength = titleResult.length;
         if (ogsLength > titleLength) {
-            console.log(`ogResultLonger: ${JSON.stringify(ogsResult)}`);
+          console.log(`ogResultLonger: ${JSON.stringify(ogsResult)}`);
           res.json({ result: ogsResult.result.ogTitle });
         } else {
-            console.log(`titleResultLonger: ${titleResult}`);
+          console.log(`titleResultLonger: ${titleResult}`);
           res.json({ result: titleResult });
         }
       }
     } else {
-        console.log(`No results`);
-      res.status(500).json({ error: 'Failed to retrieve title' });
+      console.log(`No results`);
+      res.status(500).json({ error: "Failed to retrieve title" });
     }
   };
 
@@ -62,18 +123,22 @@ app.post('/get-title', (req, res) => {
     .then((data) => {
       const { error, result } = data;
       ogsResult = { error, result };
+      console.log(`ogsResult: ${JSON.stringify(ogsResult)}`); // Add logging here
     })
     .catch((error) => {
       ogsResult = { error: true, result: error };
+      console.log(`ogsError: ${error}`); // Add logging here
     })
     .then(() => {
       title(url)
         .then((title) => {
           titleResult = title;
+          console.log(`titleResult: ${titleResult}`); // Add logging here
           checkResults();
         })
         .catch((error) => {
           titleResult = error;
+          console.log(`titleError: ${error}`); // Add logging here
           checkResults();
         });
     });
