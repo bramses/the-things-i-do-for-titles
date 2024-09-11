6bcf2be520b3e1f36b33ad2be469b8c769402623
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-09-10 21:44:51 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ogs = require("open-graph-scraper");
+const title = require("url-to-title");
+
+const app = express();
+app.use(bodyParser.json());
+
+const API_KEY = process.env.API_KEY;
+
+app.post('/get-title', (req, res) => {
+  const { url, apiKey } = req.body;
+
+  if (apiKey !== API_KEY) {
+    return res.status(403).json({ error: 'Invalid API key' });
+  }
+
+  const options = { url };
+  let ogsResult = null;
+  let titleResult = null;
+
+  const checkResults = () => {
+    if (ogsResult || titleResult) {
+      if (ogsResult.error || 'ogTitle' in ogsResult.result === false) {
+        res.json({ result: titleResult });
+      } else if (titleResult === 'Unsupported browser' || titleResult === undefined) {
+        res.json({ result: ogsResult.result.ogTitle + " - " + ogsResult.result.ogDescription });
+      } else {
+        const ogsLength = ogsResult.result.ogTitle.length;
+        const titleLength = titleResult.length;
+        if (ogsLength > titleLength) {
+          res.json({ result: ogsResult.result.ogTitle });
+        } else {
+          res.json({ result: titleResult });
+        }
+      }
+    } else {
+      res.status(500).json({ error: 'Failed to retrieve title' });
+    }
+  };
+
+  ogs(options)
+    .then((data) => {
+      const { error, result } = data;
+      ogsResult = { error, result };
+    })
+    .catch((error) => {
+      ogsResult = { error: true, result: error };
+    })
+    .then(() => {
+      title(url)
+        .then((title) => {
+          titleResult = title;
+          checkResults();
+        })
+        .catch((error) => {
+          titleResult = error;
+          checkResults();
+        });
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
